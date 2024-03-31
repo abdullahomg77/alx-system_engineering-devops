569f8a5e4445225440aef37ccf8b1230f82c0d1c
abdullahomg77 <abdullahomg77@gmail.com>
2024-03-30 19:46:48 -0700

    push

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install Flash varseion 2..1.0 using pip3
-package {'flask':
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Install Flask version 2.1.0 using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
